6a323640821be09887c55fab3d5718560a03c8ac
logonrm <logonrm@fiap.com.br>
2018-03-20 22:57:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/room/Nota.kt
@@ -0,0 +1,18 @@
+package com.example.logonrm.room
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+/**
+ * Created by André Ribeiro on 20/03/2018.
+ */
+@Entity(tableName = "nota")
+data class Nota(
+        @PrimaryKey(autoGenerate = true)
+        var id: Int?,
+        @ColumnInfo(name = "description")
+        var descricao: String
+) {
+    constructor() : this(null, "")
+}
